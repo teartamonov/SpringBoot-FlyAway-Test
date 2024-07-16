fda8e0da49cfc739e1f185ca8f3ba686bd672293
Timofey A <meiwenk46@gmail.com>
2024-07-16 21:08:14 +0300

    Tested SpringBoot & Flyway

---
+++ src/test/java/com/github/teartamonov/sprinboot_flyaway_demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.teartamonov.sprinboot_flyaway_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
